1b52b838723c759fb0e2bb30c74e9f8876d76841
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2021-09-04 14:01:40 +0200

    adds movie modal window

---
+++ src/js/movie-modal.js
@@ -0,0 +1,38 @@
+import functionName from './apiService';
+import templateName from '../hbs/modal.hbs';
+
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('btn_modal_window');
+const close = document.getElementsByClassName('close_modal_window')[0];
+
+btn.addEventListener('click', onCardClick);
+close.addEventListener('click', closeModalWindow);
+window.addEventListener('click', onWindowClick);
+
+function onCardClick(ev) {
+  ev.preventDefault();
+  modal.style.display = 'flex';
+  document.body.classList.add('modal-open');
+}
+
+function closeModalWindow(ev) {
+  modal.style.display = 'none';
+  document.body.classList.remove('modal-open');
+}
+
+function onWindowClick(ev) {
+  if (ev.target == modal) {
+    closeModalWindow();
+  }
+}
+
+window.addEventListener(
+  'keydown',
+  function (event) {
+    if (event.code === 'Escape') {
+      closeModalWindow();
+    }
+    return;
+  },
+  true,
+);
