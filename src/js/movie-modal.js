e79273237327289f0349c433bca54977e83c8506
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2021-09-05 20:12:43 +0200

    adds modal markup

--- src/js/movie-modal.js
+++ src/js/movie-modal.js
@@ -1,19 +1,49 @@
-import functionName from './api-service';
-// import templateName from '../hbs/modal.hbs';
+import ApiService from './api-service';
+import modalMovieTpl from '../templates/modal-markup.hbs';
+
+const apiService = new ApiService();
 
 const modal = document.getElementById('myModal');
-const btn = document.getElementById('btn_modal_window');
+const galleryList = document.getElementById('gallery');
+const modalContent = document.querySelector('.modal_content');
+// const btn = document.getElementById('btn_modal_window');
 const close = document.getElementsByClassName('close_modal_window')[0];
+const modalMarkup = modalMovieTpl();
 
-btn.addEventListener('click', onCardClick);
+galleryList.addEventListener('click', onCardClick);
+// btn.addEventListener('click', onCardClick);
 close.addEventListener('click', closeModalWindow);
 window.addEventListener('click', onWindowClick);
 
+function getFullMovieInfo(id) {
+  apiService.fetchMovieById(id).then(movieInfo => {
+    const markup = modalMovieTpl(movieInfo);
+    const modal = modalContent.create(markup);
+    modal.style.display = 'flex';
+    document.body.classList.add('modal-open');
+  });
+}
+
 function onCardClick(ev) {
   ev.preventDefault();
-  modal.style.display = 'flex';
-  document.body.classList.add('modal-open');
+  let id = e.target.dataset.action;
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  getFullMovieInfo(id);
+
+  // modal.style.display = 'flex';
+  // document.body.classList.add('modal-open');
 }
+
+// function createModalMovieMarkup(tempModalData) {
+//   return tempModalData.map(modalMovieTpl).join('');
+// }
+
+// function renderModalMarkup(tempModalData) {
+//   const markup = createModalMovieMarkup(tempModalData);
+//   modalContent.innerHTML = markup;
+// }
 
 function closeModalWindow(ev) {
   modal.style.display = 'none';
@@ -36,3 +66,42 @@ window.addEventListener(
   },
   true,
 );
+
+// import functionName from './api-service';
+// // import templateName from '../hbs/modal.hbs';
+
+// const modal = document.getElementById('myModal');
+// const btn = document.getElementById('btn_modal_window');
+// const close = document.getElementsByClassName('close_modal_window')[0];
+
+// btn.addEventListener('click', onCardClick);
+// close.addEventListener('click', closeModalWindow);
+// window.addEventListener('click', onWindowClick);
+
+// function onCardClick(ev) {
+//   ev.preventDefault();
+//   modal.style.display = 'flex';
+//   document.body.classList.add('modal-open');
+// }
+
+// function closeModalWindow(ev) {
+//   modal.style.display = 'none';
+//   document.body.classList.remove('modal-open');
+// }
+
+// function onWindowClick(ev) {
+//   if (ev.target == modal) {
+//     closeModalWindow();
+//   }
+// }
+
+// window.addEventListener(
+//   'keydown',
+//   function (event) {
+//     if (event.code === 'Escape') {
+//       closeModalWindow();
+//     }
+//     return;
+//   },
+//   true,
+// );
