f2f1b35eef8cddd00cfbde5c4f845ac6f0f41a3a
Kirill Romanenko <brs.group.com@gmail.com>
2021-09-06 22:45:55 +0300

    fixed scroll

--- src/js/scroll_up/scrollUp.js
+++ src/js/scroll_up/scrollUp.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
   });
 
   $('.scrollup').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 600);
+    $('html, body').animate({ scrollTop: 0 }, 200);
     return false;
   });
 });
