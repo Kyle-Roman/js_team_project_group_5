e8648b0850bc0a918d73cd231b5db0409355dde7
LukeBrutale <yuriybema@gmail.com>
2021-09-09 18:34:15 +0300

    update_preloader

--- src/js/loader.js
+++ src/js/loader.js
@@ -3,11 +3,6 @@ let preLoader = document.querySelector('#preloader');
 window.addEventListener('load', () => {
     setTimeout(() => {
         preLoader.remove();
+        $('body').removeClass('scroll-off');
     }, 1500);
 })
-
-// $(window).load(function() {
-//       $(".preloader").fadeOut(1000, function() {
-//           $('body').removeClass('loading');
-//       });
-//     });
