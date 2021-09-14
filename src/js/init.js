5435efa0613d445ee84fe0707b335020021a9f1f
Kirill Romanenko <brs.group.com@gmail.com>
2021-09-14 22:03:09 +0300

    Update init.js

--- src/js/init.js
+++ src/js/init.js
@@ -1,12 +1,12 @@
-import MoviesApiService from './trendingMovies/api.js';
+import { getTrending } from './search-trending';
 import setBaseConfig from './local-storage';
 import apiService from '..';
-const api = new MoviesApiService();
+
 export default async function Initialize() {
   await setBaseConfig();
 
-  api.resetPage();
-  api.fetchMovies();
+  apiService.resetPage();
+  getTrending('day');
 }
 
 Initialize();
